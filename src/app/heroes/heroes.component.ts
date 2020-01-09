d74628a61fb377e370f5b98593a7975cd7f9ee14
lcruz-taxsat <lizbeth.cruz@taxsat.com>
2020-01-09 15:56:50 -0600

    Detail components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heores';
+import { HEROES } from '../moke-heroes';
+import { Hero } from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
-
 })
-
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
   heroes = HEROES;
-  constructor() { }
   selectedHero: Hero;
+  constructor() { }
+  ngOnInit() {}
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  ngOnInit() {
-  }
+
+
 
 }
