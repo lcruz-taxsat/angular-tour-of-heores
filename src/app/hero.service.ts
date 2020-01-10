fe53dff8e6ca059eb80eb7727f0440e72a6dacdd
lcruz-taxsat <lizbeth.cruz@taxsat.com>
2020-01-10 15:57:27 -0600

    error on router

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,9 +4,7 @@ import { Hero } from './hero';
 import { HEROES } from './moke-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root',})
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -15,5 +13,10 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,19 +8,10 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  /*hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;*/
-  selectedHero: Hero;
   heroes: Hero[];
   constructor(private heroService: HeroService) { }
   ngOnInit() {
     this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
   getHeroes(): void {
     this.heroService.getHeroes()
